6d43f1cb6720f717893858522c2b668983febb1c
Alvin <git-commit-address>
2021-04-20 14:01:32 +0800

    updating xylophone mobile app with colors; and music files

---
+++ android/app/src/main/kotlin/sozoalvin/xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package sozoalvin.xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  //
+  void playSound(int sn) {
+    //function called playSound. Void as there are no return parameters
+    final player = AudioCache();
+    player.play('note$sn.wav');
+  }
+
+  // buildKey(color:Colors.red, soundNumber: 1),
+  Expanded buildKey({int soundNumber, Color color}) {
+    //we deliberately swapped named parameters
+    //this is a function with EXPANDED as return type
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+} //end XylophoneApp stateless widget
+
+//
